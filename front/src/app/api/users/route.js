fe044000e7e0202ca1ad90609c02907a0dd404ff
ham144 <24434muhammad.yafizham@gmail.com>
2024-04-14 23:54:25 +0000

    Apr 15, 2024, 6:54 AM

--- front/src/app/api/users/route.js
+++ front/src/app/api/users/route.js
@@ -1,5 +1,15 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function GET(req, res) {
+export const base = axios.create('localhost:2000')
 
+export const getUsers = async () => {
+    const data = base.get('/users')
+    return data
 }
+
+export default createUser = async ({ firstname, lastname, age }) => {
+    const data = base.post('/users', { firstname: firstname, lastname: lastname, age: age })
+    return data
+}
+
+
