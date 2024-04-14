09c3aa2e1389ef82185a2116b6a0ce635ae0f156
ham144 <24434muhammad.yafizham@gmail.com>
2024-04-14 23:22:04 +0000

    Apr 15, 2024, 6:22 AM

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,6 +1,5 @@
 "use client"
 import { GlobalContext } from '@/components/globalState'
-import UseReducer from '@/components/userReducer'
 import React, { useContext, useEffect } from 'react'
 
 const Home = () => {
@@ -9,7 +8,6 @@ const Home = () => {
 
   return (
     <main className='bg-slate-100 flex min-h-screen '>
-      <UseReducer />
     </main>
   )
 }
