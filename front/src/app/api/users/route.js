ebfc3475762f6ec7d0e32c9e2a784b59595dadd2
ham144 <24434muhammad.yafizham@gmail.com>
2024-04-15 00:16:12 +0000

    Apr 15, 2024, 7:16 AM

--- front/src/app/api/users/route.js
+++ front/src/app/api/users/route.js
@@ -13,3 +13,21 @@ export default createUser = async ({ firstname, lastname, age }) => {
 }
 
 
+import express from "express";
+const route = express.router()
+
+export function searchbyid(req, res, next) {
+    const { id } = req.params
+    if (!id) return res.status(402).json({ error: "no string id" })
+    res.json({ id: id })
+    return next()
+}
+
+App.use(searchbyid)
+
+route.get('/users/:id', (req, res) => {
+    const { id } = req.params()
+    return res.send()
+})
+
+
